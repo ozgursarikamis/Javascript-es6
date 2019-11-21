d35724cec53d5c0cdcb073da3d3db7f6defe51f6
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 22:33:49 +0300

    math sign

--- Ch02-Knowing Your Library/number.js
+++ Ch02-Knowing Your Library/number.js
@@ -15,10 +15,21 @@ let f = "NaN";
 // console.log(Number.isNaN(d));
 console.log("*****************************************");
 
-console.log(isFinite(10));
-console.log(isFinite([]));
-console.log();
+// console.log(isFinite(10));
+// console.log(isFinite([]));
+// console.log();
 
-console.log(Number.isFinite(10));
-console.log(Number.isFinite([]));
+// console.log(Number.isFinite(10));
+// console.log(Number.isFinite([]));
 
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+function epsilonEqual(a, b) {
+    return Math.abs(a - b) < Number.EPSILON;
+}
+
+console.log(epsilonEqual(0.1 + 0.2, 0.3));
+console.log(Math.sign(-2));
+console.log(Math.sign(11));
+console.log(Math.sign(0));
