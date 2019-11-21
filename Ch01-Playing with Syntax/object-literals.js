c4c387f5ded4993fbd129877fc1435322e8c6f4e
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 15:32:21 +0300

    object literals

---
+++ Ch01-Playing with Syntax/object-literals.js
@@ -0,0 +1,12 @@
+// IN ES5:
+var x = 1, y = 2;
+var o = {
+    x: x, y: y
+}
+console.log('o', o);
+console.log('o.x', o.x);
+
+// IN ES6:
+let xx = 1, yy = 2;
+let object = { xx, yy };
+console.log('object', object);
