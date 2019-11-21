2972e8e8a75f566a3266c7ba3709613d27472858
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 11:55:20 +0300

    closure samples

--- closures/app.js
+++ closures/app.js
@@ -19,4 +19,22 @@ function makeFunc() {
 }
 
 var myFunc = makeFunc();
-myFunc();
+// myFunc();
+
+function makeAdder(x) {
+    return function (y) {
+        return x + y;
+    }
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log('add5(2):', add5(2));
+console.log('add10(2)', add10(2));
+
+/*
+    add5 and add10 are both closures. 
+    They share the same function body definition, 
+    but store different lexical environments. 
+*/
