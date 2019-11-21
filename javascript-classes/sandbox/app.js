3cf3098959dcdcdb28d983065a18eb3967b98c56
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 10:49:48 +0300

    mixins

--- javascript-classes/sandbox/app.js
+++ javascript-classes/sandbox/app.js
@@ -1,3 +1,16 @@
+let mixin = {
+    madeIn(){
+        console.log('This car was made in year 2019!');
+    }
+}
+
+let carMixin = {
+    __proto__: mixin,
+    madeIn() {
+        super.madeIn();
+    }
+}
+
 class Car {
     constructor(doors, engine, color){
         this.doors = doors;
@@ -22,13 +35,15 @@ class SUV extends Car {
         this.brand = brand;
         this.wheels = 4;
         this.ac = true;
+
+        // assign mixin:
+        Object.assign(this, carMixin);
     }
 
     myBrand(){
         return console.log(`This SUV is a ${this.brand}`)
     }
 }
-
 
 const cx5 = new Car(5, 'v6', 'grey');
 const civic = new SUV(3, 'v4', 'blue');
@@ -37,6 +52,7 @@ const suv = new SUV(5, 'v8', 'black', 'Volvo');
 console.log('suv', suv);
 console.log('cx5', cx5);
 console.log(suv.myBrand());
+console.log('suv mixin', suv.madeIn());
 console.log('civic', cx5);
 // console.log('cx5 Stats:', cx5.carStats());
 // console.log(Car.totalDoors(cx5, civic));
