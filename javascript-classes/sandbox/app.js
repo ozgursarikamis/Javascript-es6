4e5a08e9ceca4149c147d21a92c76717f2907f3c
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 10:06:54 +0300

    hoisting

--- javascript-classes/sandbox/app.js
+++ javascript-classes/sandbox/app.js
@@ -8,7 +8,13 @@ class Car {
         return `This car has ${this.doors} doors, a ${this.engine} and a beautiful ${this.color}!`
     }
 }
-
+sayHi();
 const cx5 = new Car(5, 'v6', 'grey');
 console.log('cx5', cx5);
 console.log('cx5 Stats:', cx5.carStats());
+
+function sayHi() {
+    return console.log("This function can be called anywhere");
+}
+
+sayHi();
