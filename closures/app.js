6c4c9fc792cd25836469352865ee4d186cdf45d3
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 11:58:12 +0300

    using global scope with closures

--- closures/app.js
+++ closures/app.js
@@ -30,11 +30,27 @@ function makeAdder(x) {
 var add5 = makeAdder(5);
 var add10 = makeAdder(10);
 
-console.log('add5(2):', add5(2));
-console.log('add10(2)', add10(2));
+// console.log('add5(2):', add5(2));
+// console.log('add10(2)', add10(2));
 
 /*
     add5 and add10 are both closures. 
     They share the same function body definition, 
     but store different lexical environments. 
 */
+
+// global scope
+var e = 10;
+function sum(a) {
+    return function (b) {
+        return function (c) {
+            // outer functions scope
+            return function (d) {
+                // local scope
+                return a + b + c + d + e;
+            }
+        }
+    }
+}
+console.log(sum(1)(2)(3)(4)); // log 20
+console.log(sum(4)(3)(2)(1)); // log 20
