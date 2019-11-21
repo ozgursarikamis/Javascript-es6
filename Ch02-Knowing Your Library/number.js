ab6cde540dede6d47ef42cc6610f61dccaab4764
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 22:25:28 +0300

    isFinite

--- Ch02-Knowing Your Library/number.js
+++ Ch02-Knowing Your Library/number.js
@@ -1,15 +1,24 @@
 let a = 17.0;
 let b = 1.2;
 
-console.log('Number.isInteger(a) :', Number.isInteger(a));
-console.log('Number.isInteger(b) :', Number.isInteger(b));
+// console.log('Number.isInteger(a) :', Number.isInteger(a));
+// console.log('Number.isInteger(b) :', Number.isInteger(b));
 
 let c = "hello";
 let d = 12;
 let e = NaN;
 let f = "NaN";
 
-console.log(Number.isNaN(e));
-console.log(Number.isNaN(f));
-console.log(Number.isNaN(c));
-console.log(Number.isNaN(d));
+// console.log(Number.isNaN(e));
+// console.log(Number.isNaN(f));
+// console.log(Number.isNaN(c));
+// console.log(Number.isNaN(d));
+console.log("*****************************************");
+
+console.log(isFinite(10));
+console.log(isFinite([]));
+console.log();
+
+console.log(Number.isFinite(10));
+console.log(Number.isFinite([]));
+
