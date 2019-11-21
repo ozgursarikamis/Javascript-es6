cbb9fbea43bcd9bb5f69403a18dba602c06d867e
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 15:23:11 +0300

    object destruction

--- Ch01-Playing with Syntax/object-destructing.js
+++ Ch01-Playing with Syntax/object-destructing.js
@@ -1,5 +1,8 @@
 var o = { name: "John", age: 23 };
-const {name, age} = o;
-console.log('name', name);
-console.log('age', age);
+// const {name, age} = o;
+// console.log('name', name);
+// console.log('age', age);
 
+let {name: x, age: y} = o;
+console.log('x', x);
+console.log('y', y);
