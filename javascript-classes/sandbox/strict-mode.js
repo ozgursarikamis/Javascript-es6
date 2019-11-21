1803cd3189db866143fe15288d0ef35c87dcfb6a
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 10:19:38 +0300

    strict mode

---
+++ javascript-classes/sandbox/strict-mode.js
@@ -0,0 +1,9 @@
+"use strict"
+
+var undefined = 5; // TypeError
+var Infinity = 4; // TypeError
+
+console.log('undefined', undefined);
+console.log('Infinity', Infinity);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
