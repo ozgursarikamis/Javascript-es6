4da641fc3f34196b9562befa7ba0c21d4ac1be4a
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-21 22:47:36 +0300

    String.raw

--- Ch02-Knowing Your Library/strings.js
+++ Ch02-Knowing Your Library/strings.js
@@ -2,3 +2,14 @@ console.log("a".repeat(9));
 const str = "Hi. I'm a Javascript Developer";
 console.log(str.includes("Javascript"));
 console.log(str.includes("Javascript", 11));
+
+console.log(str.startsWith("Hi"));
+console.log(str.endsWith("Developer"));
+
+console.log("1\n2\n3");
+console.log(`1
+2
+3`);
+
+let s = String.raw `xy\/*{{|||n${ 1 + 1 }z`;
+console.log(s);
