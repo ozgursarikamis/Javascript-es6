d5f00097005b5e8fed466b44cfb465d6e86e68e2
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-22 08:56:26 +0300

    copyWithin

--- Ch02-Knowing-Your-Library/arrays-entries.js
+++ Ch02-Knowing-Your-Library/arrays-entries.js
@@ -6,3 +6,8 @@ let values = arr.values();
 console.log('entries', ...entries);
 console.log('keys', ...keys);
 console.log('values', ...values);
+console.log();
+
+let arr1 = [1, 2, 3, 4, 5];
+arr1.copyWithin(1, 2, 4);
+console.log(arr1);
