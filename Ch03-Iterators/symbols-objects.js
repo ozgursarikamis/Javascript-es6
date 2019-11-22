6a3aa591fe810fcba2a98a3551c25fbb3b9d4d36
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-22 16:53:22 +0300

    symbols & objects

---
+++ Ch03-Iterators/symbols-objects.js
@@ -0,0 +1,10 @@
+let obj = { a: 12 };
+let s1 = Symbol("my symbol");
+let s2 = Symbol("my symbol");
+
+Object.defineProperty(obj, s1, {
+    enumerable: false
+});
+
+obj[s2] = "";
+console.log(Object.getOwnPropertySymbols(obj));
