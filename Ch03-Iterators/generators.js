726bb0e3b4ab4aa635dcdd9170496bd774065b22
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-25 13:23:54 +0300

    generators 2

--- Ch03-Iterators/generators.js
+++ Ch03-Iterators/generators.js
@@ -6,29 +6,43 @@ function* generator() {
     yield 5;
 }
 
-let gen = generator();
+// let gen = generator();
 
-console.log('generator 1', {
-    value : gen.next().value
-}); 
+// console.log('generator 1', {
+//     value : gen.next().value
+// }); 
 
-console.log('generator 2', {
-    value : gen.next().value
-}); 
+// console.log('generator 2', {
+//     value : gen.next().value
+// }); 
 
-console.log('generator 3', {
-    value : gen.next().value
-}); 
+// console.log('generator 3', {
+//     value : gen.next().value
+// }); 
 
-console.log('generator 4', {
-    value : gen.next().value
-}); 
+// console.log('generator 4', {
+//     value : gen.next().value
+// }); 
 
-console.log('generator 5', {
-    value : gen.next().value
-}); 
-if (!gen.next().done) {    
-    console.log('generator 1', {
-        value : gen.next().value
-    });  
+// console.log('generator 5', {
+//     value : gen.next().value
+// }); 
+// if (!gen.next().done) {    
+//     console.log('generator 1', {
+//         value : gen.next().value
+//     });  
+// }
+
+function* generatorFunc() {
+    var a = yield 12;
+    var b = yield a + 1;
+    var c = yield b + 2;
+    yield c + 3;
 }
+
+var generator = generatorFunc();
+console.log(generator.next());
+console.log(generator.next(1));
+console.log(generator.next(2));
+console.log(generator.next(3));
+console.log(generator.next(4));
