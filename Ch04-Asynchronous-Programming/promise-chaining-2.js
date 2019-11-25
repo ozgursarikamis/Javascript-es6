7e89a1b6d1c4846b7321bd68b45564ad1815ad15
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-11-25 22:56:09 +0300

    promise finally

--- Ch04-Asynchronous-Programming/promise-chaining-2.js
+++ Ch04-Asynchronous-Programming/promise-chaining-2.js
@@ -1,6 +1,6 @@
 const loader = document.getElementById("loading");
 
-loader.style.display = "inline-block";
+// loader.style.display = "inline-block";
 
 let p1 = new Promise(function(resolve, reject) {
     setTimeout(resolve, 500, 'p1');
@@ -23,14 +23,29 @@ let p5 = new Promise(function(resolve, reject) {
 });
 
 
+// let promise = Promise.all([
+//     p1, p2, p3, p4, p5
+// ]);
 let promise = Promise.all([
-    p1, p2, p3, p4, p5
+    p1.catch(function() {}), 
+    p2.catch(function() {}), 
+    p3.catch(function() {}), 
+    p4.catch(function() { 
+        // loader.style.display = "none"; 
+    }), 
+    p5.catch(function() {})
 ]);
 
+
+// loader.style.display = "inline-block";
 promise.then(data => {
-    loader.style.display = "inline-block";
     data.forEach(d => console.log('d :', d));
 }).catch(error => {
-    loader.style.display = "none";
+    // loader.style.display = "none";
     console.error(error);
+}).finally(() => {
+    const hide = confirm("hide loading?");
+    if (hide) {
+        loader.style.display = "none";
+    }
 });
